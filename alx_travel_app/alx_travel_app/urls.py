b2d2b8983e35e0b3e741cc5dae98f1ee6bfb0906
Precious <banigoprecious@yahoo.com>
2025-02-07 11:58:56 +0100

    Milestone 3: Creating Views and API Endpoints

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -15,19 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-# alx _travel_app Swagger Integration
-
-from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('listings.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
+
+# alx_travel_app_0x01/urls.py
+
+
+# alx _travel_app Swagger Integration
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -41,8 +45,3 @@ schema_view = get_schema_view(
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
